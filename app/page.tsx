90a76c84cf86b38ee78c9c3ca641aa2af62dec3d
Ashish <ashishadhikari5255@gmail.com>
2024-11-01 11:52:38 +0545

    Add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1 className=" h1-bold background-light850_dark100 ">Hello world</h1>
+    </div>
+  );
 };
 
 export default Home;
